a2ac1535cf208c788bd786392babe38edcff6f00
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-05-11 18:23:34 -0500

    moving over to new computer

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 let apiKey = '8eb95ffe3a74bb5ab411107a63ac6c09';
+
+let searchButton = document.getElementById(searchButton)
+let cityName = document.getElementById(searchName)
+let desiredCity = docoment.getElementById(desiredCity)
+
+
