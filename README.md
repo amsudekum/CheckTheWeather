ba0f65a30b1790456287592de884e289c5087714
amsudekum <116687202+amsudekum@users.noreply.github.com>
2023-05-11 19:19:27 -0500

    wrote a readme and added event listeners

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ## Motivation 
 Nothing is worse than forgetting to go out of town, find out it's raining the entire duration of your trip, and realize you didn't pack a jacket. Now you'll be able to enter the location of your destination and get a 5 day weather forecast. 
 
+## Installation 
+Visit the application at this link: 
+
--- script.js
+++ script.js
@@ -2,6 +2,19 @@ let apiKey = '8eb95ffe3a74bb5ab411107a63ac6c09';
 
 let searchButton = document.getElementById(searchButton)
 let cityName = document.getElementById(searchName)
-let desiredCity = docoment.getElementById(desiredCity)
+let desiredCity = document.getElementById(desiredCity)
 
+let cities = [];
 
+//Use the search button 
+searchButton.addEventListener('click', function() {
+    let city = cityName.value;
+    desiredCity.text = 'Desired city: ' + city;
+})
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    let city = cityName.value;
+    desiredCity.textContent = 'Desired city: ' + city;
+    form.reset();
+})
